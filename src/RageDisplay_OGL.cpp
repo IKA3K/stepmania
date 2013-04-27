290b26f282f7679c8db1f6b3540b902422460222
Jason Felds <jafelds@gmail.com>
2013-04-27 14:02:11 -0400

    All set loops handled.

--- src/RageDisplay_OGL.cpp
+++ src/RageDisplay_OGL.cpp
@@ -1055,8 +1055,8 @@ public:
 
 static void InvalidateObjects()
 {
-	FOREACHS( InvalidateObject*, g_InvalidateList, it )
-		(*it)->Invalidate();
+	for (InvalidateObject *it : g_InvalidateList)
+		it->Invalidate();
 }
 
 class RageCompiledGeometryHWOGL : public RageCompiledGeometrySWOGL, public InvalidateObject
@@ -2442,7 +2442,6 @@ void RenderTarget_FramebufferObject::Create( const RenderTargetParam &param, int
 		glGenRenderbuffersEXT( 1, reinterpret_cast<GLuint*>(&m_iDepthBufferHandle) );
 		ASSERT( m_iDepthBufferHandle != 0 );
 
-		glBindRenderbufferEXT( GL_RENDERBUFFER, m_iDepthBufferHandle );
 		glRenderbufferStorageEXT( GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT16, iTextureWidth, iTextureHeight );
 		glFramebufferRenderbufferEXT( GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, m_iDepthBufferHandle );
 	}
