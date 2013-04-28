35c805fc5c150a479f7f9dfffff9ff812061f5fa
Jason Felds <jafelds@gmail.com>
2013-04-28 16:46:25 -0400

    Apply for each loops.

--- src/Model.cpp
+++ src/Model.cpp
@@ -731,44 +731,44 @@ void Model::Update( float fDelta )
 int Model::GetNumStates() const
 {
 	int iMaxStates = 0;
-	FOREACH_CONST( msMaterial, m_Materials, m )
-		iMaxStates = max( iMaxStates, m->diffuse.GetNumStates() );
+	for (msMaterial const &m : m_Materials)
+		iMaxStates = max( iMaxStates, m.diffuse.GetNumStates() );
 	return iMaxStates;
 }
 
 void Model::SetState( int iNewState )
 {
-	FOREACH( msMaterial, m_Materials, m )
+	for (msMaterial &m : m_Materials)
 	{
-		m->diffuse.SetState( iNewState );
-		m->alpha.SetState( iNewState );
+		m.diffuse.SetState( iNewState );
+		m.alpha.SetState( iNewState );
 	}
 }
 
 void Model::RecalcAnimationLengthSeconds()
 {
-	m_animation_length_seconds= 0;
-	FOREACH_CONST(msMaterial, m_Materials, m)
+	m_animation_length_seconds = 0;
+	for (msMaterial const &m : m_Materials)
 	{
 		m_animation_length_seconds= max(m_animation_length_seconds,
-			m->diffuse.GetAnimationLengthSeconds());
+			m.diffuse.GetAnimationLengthSeconds());
 	}
 }
 
 void Model::SetSecondsIntoAnimation( float fSeconds )
 {
-	FOREACH( msMaterial, m_Materials, m )
+	for (msMaterial &m : m_Materials)
 	{
-		m->diffuse.SetSecondsIntoAnimation( fSeconds );
-		m->alpha.SetSecondsIntoAnimation( fSeconds );
+		m.diffuse.SetSecondsIntoAnimation( fSeconds );
+		m.alpha.SetSecondsIntoAnimation( fSeconds );
 	}
 }
 
 bool Model::MaterialsNeedNormals() const
 {
-	FOREACH_CONST( msMaterial, m_Materials, m )
+	for (msMaterial const &m : m_Materials)
 	{
-		if( m->NeedsNormals() )
+		if( m.NeedsNormals() )
 			return true;
 	}
 	return false;
